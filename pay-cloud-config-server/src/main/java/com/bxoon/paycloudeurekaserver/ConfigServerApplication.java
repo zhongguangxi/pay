85ef1f66c2a53b00613faa057fc0f4afd0f9519e
qq32933432 <32933432@qq.com>
2019-04-17 21:29:31 +0800

    first commit

--- pay-cloud-config-server/src/main/java/com/bxoon/paycloudeurekaserver/ConfigServerApplication.java
+++ pay-cloud-config-server/src/main/java/com/bxoon/paycloudeurekaserver/ConfigServerApplication.java
@@ -1,7 +1,8 @@
-package com.bxoon.paycloudconfigserver;
+package com.bxoon.paycloudeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
---
+++ pay-cloud-eureka-server/src/main/java/com/bxoon/paycloudeurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.bxoon.paycloudeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 32933
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
