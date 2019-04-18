3cd39337aab0bf38182feb9bfa10a4c27c1e5a0d
qq32933432 <32933432@qq.com>
2019-04-18 17:14:29 +0800

    first commit

---
+++ pay-cloud-service-zuul/src/main/java/com/pay/paycloudservicezuul/PayCloudServiceZuulApplication.java
@@ -0,0 +1,19 @@
+package com.pay.paycloudservicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class PayCloudServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayCloudServiceZuulApplication.class, args);
+    }
+
+}
---
+++ pay-cloud-service-zuul/src/test/java/com/pay/paycloudservicezuul/PayCloudServiceZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pay.paycloudservicezuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PayCloudServiceZuulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
