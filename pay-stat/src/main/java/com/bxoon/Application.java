884a2d0545d1b6ad960a57705bbd0d1919be794f
qq32933432 <32933432@qq.com>
2019-04-19 20:03:17 +0800

    first commit

--- pay-stat/src/main/java/com/bxoon/Application.java
+++ pay-stat/src/main/java/com/bxoon/Application.java
@@ -2,8 +2,13 @@ package com.bxoon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
+
 public class Application {
 
 	public static void main(String[] args){
--- pay-stat/src/main/java/com/bxoon/controller/UserController.java
+++ pay-stat/src/main/java/com/bxoon/controller/UserController.java
@@ -4,13 +4,15 @@ import com.bxoon.bean.PayUser;
 import com.bxoon.dao.PayUserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RefreshScope
 public class UserController {
 
     @Autowired
