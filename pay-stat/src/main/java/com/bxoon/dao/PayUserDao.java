30a3e7a68cd19c5886bbf6df9eff3137cc1c29a1
qq32933432 <32933432@qq.com>
2019-04-21 14:04:14 +0800

    first commit

--- pay-stat/src/main/java/com/bxoon/dao/PayUserDao.java
+++
@@ -1,8 +0,0 @@
-package com.bxoon.dao;
-
-import com.bxoon.bean.PayUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PayUserDao extends JpaRepository<PayUser, Integer> {
-
-}
